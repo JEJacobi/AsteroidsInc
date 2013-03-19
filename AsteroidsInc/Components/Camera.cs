5298ae5805b46a56fe61794e6c7a80567cd69420
Jaedon Jacobi <jaedon@jacobi.io>
2013-03-18 21:02:19 -0700

    First REAL commit!
    Basic implementations of the Sprite and Camera classes, about an hours
    work.

---
+++ AsteroidsInc/Components/Camera.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace AsteroidsInc
+{
+    public static class Camera
+    {
+        #region Declarations
+
+        public static Vector2 Position = Vector2.Zero; //Default of upper left camera position
+        public static Vector2 ViewportSize = Vector2.Zero; //Default of 0 viewport size
+        public static Rectangle WorldRectangle = new Rectangle(0, 0, 0, 0); //Default of empty rectangle
+
+        public static Rectangle Viewport //returns a rectangle showing the viewport
+        {
+            get {
+                return new Rectangle((int)Position.X, (int)Position.Y, 
+                    (int)ViewportSize.X, (int)ViewportSize.Y); //cast floats to ints for rectangle init
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static void Move(Vector2 offset) //Offset camera method
+        {
+            Position += offset;
+        }
+
+        public static bool IsObjectVisible(Rectangle obj)
+        { return Viewport.Intersects(obj); } //Check if obj intersects with the viewport
+
+        public static Vector2 GetLocalCoords(Vector2 point)
+        { return point - Position; } //Transform point to position - used in converting to screen coords
+
+        public static Rectangle GetLocalCoords(Rectangle rectangle) //Overload of previous, takes a rectangle
+        {
+            return new Rectangle(
+                rectangle.Left - (int)Position.X, //get the upper left value
+                rectangle.Top - (int)Position.Y, //and upper right value
+                rectangle.Width, //return given width
+                rectangle.Height); //return given height
+        }
+
+        #endregion
+    }
+}
---
+++ AsteroidsInc/Components/Sprite.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace AsteroidsInc
+{
+    class Sprite //TODO: Add animation logic/methods
+    {
+        #region Declarations
+
+        public Texture2D Texture;
+        public Vector2 WorldLocation = Vector2.Zero; //Upper left world location
+        public Vector2 Velocity = Vector2.Zero; //No velocity
+        public Vector2 Origin = Vector2.Zero; //Default upper left origin
+        public Rectangle? SourceRectangle = null; //Default null to use entire texture
+        public Color TintColor = Color.White; //Default no tint
+        public float Rotation = 0f; //Zero rotation
+        public float Scale = 1f; //1:1 scale
+        public float Depth = 0f; //Default depth
+        public SpriteEffects Effects = SpriteEffects.None; //Default to no effects
+        public int CollisionRadius = 0;
+        public int BoundingXPadding = 0;
+        public int BoundingYPadding = 0;
+
+        #endregion
+
+        #region Properties
+
+        public Rectangle WorldRectangle
+        {
+            get
+            {
+                return new Rectangle(
+                    (int)WorldLocation.X,
+                    (int)WorldLocation.Y,
+                    Texture.Width,  //TODO: Replace with spritesheet animation logic
+                    Texture.Height);
+            }
+        }
+
+        public Rectangle BoundingBox
+        {
+            get
+            {
+                return new Rectangle(
+                    (int)WorldLocation.X,
+                    (int)WorldLocation.Y,
+                    Texture.Width, //TODO: Replace with spritesheet animation logic
+                    Texture.Height);
+            }
+        }
+
+        public Vector2 ScreenLocation
+        { get { return Camera.GetLocalCoords(WorldLocation); } } //get screen coordinates
+
+        public Rectangle ScreenRectangle
+        { get { return Camera.GetLocalCoords(WorldRectangle); } } //get screen rectangle
+
+        public Vector2 SpriteCenter
+        { get { return new Vector2(Texture.Width, Texture.Height); } } //get sprite's center
+
+        public Vector2 WorldCenter
+        { get { return WorldLocation + SpriteCenter; } } //gets the center of the sprite in world coords
+
+        public Vector2 ScreenCenter
+        { get { return Camera.GetLocalCoords(WorldLocation + SpriteCenter); } } //returns the center in screen coords
+
+        #endregion
+
+        #region Constructors
+
+        public Sprite(Texture2D texture, Vector2 worldLocation) //Quick-n-dirty simple constructor
+        {
+            if (texture == null) { throw new NullReferenceException("Null texture reference."); } //check if texture is null
+            Texture = texture; WorldLocation = worldLocation; //and assign
+        }
+
+        public Sprite( //Full constructor with optional parameters
+            Texture2D texture,
+            Vector2 worldLocation,
+            Vector2 origin,
+            Color tintColor,
+            float rotation = 0f, //default to no rotation
+            float scale = 1f, //default to 1:1 scale
+            float depth = 0f, //default to 0 layerDepth
+            Rectangle? sourceRect = null, //default to full texture
+            int collisionRadius = 0, //collision radius used in bounding circle collision
+            int xPadding = 0, //amount of x padding, used in bounding box collision
+            int yPadding = 0, //amount of y padding, used in bounding box collision
+            SpriteEffects effects = SpriteEffects.None) //default to no spriteeffects
+        {
+            if (texture == null) { throw new NullReferenceException("Null texture reference."); }
+            Texture = texture; WorldLocation = worldLocation; //assign basics
+            Origin = origin; TintColor = tintColor; //assign required params
+            Rotation = rotation; Scale = scale; Depth = depth;
+            BoundingXPadding = xPadding; BoundingYPadding = yPadding; CollisionRadius = collisionRadius;
+            SourceRectangle = sourceRect; Effects = effects; //assign optional params
+        }
+
+        #endregion
+
+        #region Methods
+
+        public bool IsBoxColliding(Rectangle obj) //TODO: Add padding values
+        {
+            return BoundingBox.Intersects(obj);
+        }
+
+        public bool IsCircleColliding(Vector2 objCenter, float objRadius)
+        {
+            if (Vector2.Distance(WorldCenter, objCenter) <
+                (CollisionRadius + objRadius))  //if the distance between centers is greater than the sum
+                return true;                    //of the radii, collision has occurred
+            else
+                return false; //if not, return false
+        }
+
+        #endregion
+    }
+}
