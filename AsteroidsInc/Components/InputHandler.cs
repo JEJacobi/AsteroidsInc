13b1a88034a506432011518b12ce687c4b5d3d1a
Jaedon Jacobi <jaedon@jacobi.io>
2013-03-19 19:02:13 -0700

    Added Mouse support to the InputHandler class.

--- AsteroidsInc/Components/InputHandler.cs
+++ AsteroidsInc/Components/InputHandler.cs
@@ -16,14 +16,16 @@ namespace AsteroidsInc.Components
     {
         public static KeyboardState CurrentKeyboardState = new KeyboardState();
         public static KeyboardState LastKeyboardState = new KeyboardState();
-
-        #region Methods
+        public static MouseState MouseState = new MouseState();
 
         public static void Update() //get new states and move current State to last State
         {
             LastKeyboardState = CurrentKeyboardState;
             CurrentKeyboardState = Keyboard.GetState();
+            MouseState = Mouse.GetState();
         }
+
+        #region Keyboard Methods
 
         public static bool IsNewKeyPress(Keys key)
         {
@@ -47,6 +49,78 @@ namespace AsteroidsInc.Components
                 return true;
             else
                 return false;
+        }
+        
+        #endregion
+
+        #region Mouse Methods
+
+        public static bool IsLeftClicking()
+        {
+            if (MouseState.LeftButton == ButtonState.Pressed)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool IsRightClicking()
+        {
+            if (MouseState.RightButton == ButtonState.Pressed)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool IsMiddleClicking()
+        {
+            if (MouseState.MiddleButton == ButtonState.Pressed)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool IsMouseOverObject(Rectangle obj) //checks if the mouse is over the object
+        {
+            if (GetMousePos().Intersects(obj))
+                return true;
+            else
+                return false;
+        }
+
+        public static bool IsClickingObject(Rectangle obj, MouseButtons button) //checks if the mouse is over the object
+        {
+            switch (button)
+            {
+                case MouseButtons.LeftMouseButton:
+                    if (IsMouseOverObject(obj) && MouseState.LeftButton == ButtonState.Pressed)
+                        return true;
+                    return false;
+                case MouseButtons.MiddleMouseButton:
+                    if (IsMouseOverObject(obj) && MouseState.MiddleButton == ButtonState.Pressed)
+                        return true;
+                    return false;
+                case MouseButtons.RightMouseButton:
+                    if (IsMouseOverObject(obj) && MouseState.RightButton == ButtonState.Pressed)
+                        return true;
+                    return false;
+                default:
+                    throw new InvalidOperationException("Invalid button specified.");
+            }
+        }
+
+        public static Rectangle GetMousePos() //returns a 1 by 1 rectangle of the mouse location
+        {
+            return new Rectangle(
+                MouseState.X,
+                MouseState.Y,
+                1, 1);
+        }
+
+        public enum MouseButtons
+        {
+            LeftMouseButton,
+            MiddleMouseButton,
+            RightMouseButton
         }
 
         #endregion
